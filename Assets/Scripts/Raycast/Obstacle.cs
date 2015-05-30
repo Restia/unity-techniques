c8374c2d406b332999007be5cd75024412eaf2db
Restia <akahi.void@gmail.com>
2015-05-30 11:58:48 +0700

    tech 02: raycasting

---
+++ Assets/Scripts/Raycast/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+    public int Id;
+
+    void OnPressed()
+    {
+        Debug.Log("OnPressed " + Id);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Raycast/RaycastObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosW = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit2D hittedObj = Physics2D.Raycast(mousePosW, Vector2.zero, 100.0f);
+            if (hittedObj.collider != null)
+            {
+                hittedObj.collider.gameObject.SendMessage("OnPressed");
+            }
+            else Debug.Log("coming through");
+        }
+	}
+}
